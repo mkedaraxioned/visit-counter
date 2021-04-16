37a5c591fca650e798c9c8e945222a9f5b16236e
mkedaraxioned <kedarm@prdxn.com>
2021-04-16 12:00:07 +0530

    counter working seperately

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,31 @@
 
 */
 
+$(document).ready(function() {
+    var counter1 = $(".counter1");
+    var counter2 = $(".counter2");
+    var counter3 = $(".counter3");
+    var max1 = 2523,max2=63075,max3=12218;
+    var max =[2523,63075,12218];
 
+
+    var counter =[counter1,counter2,counter3];
+    for(var i=0;i<max.length;i++)
+    {
+        start_counter(counter[i],max[i]);
+    }
+
+    function start_counter(counter,max) {
+        var current = parseInt(counter.html());
+        counter.text(current+10);
+        if (current< max-10) {
+            setTimeout(start_counter,10);
+          current++; 
+        }
+    }      
+    
+
+});
 
 
 
